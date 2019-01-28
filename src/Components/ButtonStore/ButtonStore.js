ad34bf96f8143318754abc01d12cb9a806c7c666
Krystian Pietroń <krystianpietr@gmail.com>
2019-01-28 10:32:03 +0100

    add connect to ButtonStore

--- src/Components/ButtonStore/ButtonStore.js
+++ src/Components/ButtonStore/ButtonStore.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Paper from 'material-ui/Paper'
 import Button from '../../Elements/Button'
+import { connect } from 'react-redux'
 import './ButtonStore.css'
-import { NavigationFullscreenExit } from 'material-ui/svg-icons';
 
 const style = {
     buttons: {
@@ -16,53 +16,57 @@ const style = {
 }
 const ButtonStore = props => (
     <Paper className='paper__ButtonStore'
-    zDepth={5}
+        zDepth={5}
     >
-            <Button
-                style={style.buttons}
-                label=" 100zł"
-                backgroundColor="gray"
-            />
-            <Button
-                style={style.buttons}
-                label="1 000zł"
-                backgroundColor="gray"
-            />
-            <Button
-                style={style.buttons}
-                label=" 10 000zł"
-                backgroundColor="gray"
-            />
-            <Button
-                style={style.buttons}
-                label=" 100 000zł"
-                backgroundColor="gray"
-            />
-            <Button
-                style={style.buttons}
-                label="1 000 000zł"
-                backgroundColor="gray"
-            />
-            <Button
-                style={style.buttons}
-                label=" 10 000 000zł"
-                backgroundColor="gray"
-            />
-            <Button
-                style={style.buttons}
-                label="100 000 000zł"
-                backgroundColor="gray"
-            />
-            <Button
-                style={style.buttons}
-                label="1 000 000 000zł"
-                backgroundColor="gray"
-            />
-            <Button
-                style={style.buttons}
-                label="Tablica Rekordów"
-                backgroundColor="gray"
-            />
+        <Button
+            style={style.buttons}
+            label=" 100zł"
+            backgroundColor="gray"
+        />
+        <Button
+            style={style.buttons}
+            label="1 000zł"
+            backgroundColor="gray"
+        />
+        <Button
+            style={style.buttons}
+            label=" 10 000zł"
+            backgroundColor="gray"
+        />
+        <Button
+            style={style.buttons}
+            label=" 100 000zł"
+            backgroundColor="gray"
+        />
+        <Button
+            style={style.buttons}
+            label="1 000 000zł"
+            backgroundColor="gray"
+        />
+        <Button
+            style={style.buttons}
+            label=" 10 000 000zł"
+            backgroundColor="gray"
+        />
+        <Button
+            style={style.buttons}
+            label="100 000 000zł"
+            backgroundColor="gray"
+        />
+        <Button
+            style={style.buttons}
+            label="1 000 000 000zł"
+            backgroundColor="gray"
+        />
+        <Button
+            style={style.buttons}
+            label="Tablica Rekordów"
+            backgroundColor="gray"
+        />
     </Paper>
 )
-export default ButtonStore
+
+const mapDispatchToProps = dispatch => ({
+
+})
+export default connect()(ButtonStore)
